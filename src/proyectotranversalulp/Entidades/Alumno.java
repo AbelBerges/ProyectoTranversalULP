5278087b2e626daec6a3d0af66ecac01fae470e3
Tomas Abatedaga Biole <abatedagatomas@gmail.com>
2023-09-11 10:35:52 -0300

    actualizamos clase alumno

--- src/proyectotranversalulp/Entidades/Alumno.java
+++ src/proyectotranversalulp/Entidades/Alumno.java
@@ -4,10 +4,69 @@
  */
 package proyectotranversalulp.Entidades;
 
+import java.util.Date;
+
 /**
  *
  * @author kalema
  */
 public class Alumno {
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private Date fechaNacimiento;
+    private int estado;
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, String apellido, int dni, Date fechaNacimiento, int estado) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+    
     
 }
