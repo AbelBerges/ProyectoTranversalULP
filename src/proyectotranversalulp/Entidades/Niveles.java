5bb0d0572dbfef4832bb54c8acd87d020f3fdd11
Manuel Vasquez <manuvasquez2004@gmail.com>
2023-09-11 11:19:58 -0300

    Clase Niveles añadida
    clase Niveles tiene constructor y getters y setters

--- src/proyectotranversalulp/Entidades/Niveles.java
+++ src/proyectotranversalulp/Entidades/Niveles.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyectotranversalulp.Entidades;
-
 /**
  *
- * @author kalema
- */
+ * @author Manuel Vasquez
+ * 
+ **/
 public class Niveles {
+    private int idNivel;
+    private String nombre;
+    private String descripcion;
+    private boolean estado;
+    
+    public Niveles(){}
+    
+    public Niveles(int idNivel, String nombre, String descripcion, boolean estado){
+        this.idNivel = idNivel;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public int getIdNivel() {
+        return idNivel;
+    }
+    public void setIdNivel(int idNivel) {
+        this.idNivel = idNivel;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public boolean isEstado() {
+        return estado;
+    }
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
     
 }
